e9bfd838473366bcdb2d422af6394da32d48ff34
Pure Krome <github@world-domination.com.au>
2015-07-26 11:10:34 +1000

    :sparkles: We haz a ReadMe!

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Xamarin Forms - Starter Kit
+
+This is a common template for Xamarin Form applications. Why keep re-inventing the same first pages, views and authentication?  
+Start your new app, based off this template.  
+Includes:
+- Login screen (if not logged in) for Facebook/Google auth.
+- Dashboard landing page once authenticated.
+
+---
+[![I'm happy to accept tips](http://img.shields.io/gittip/purekrome.svg?style=flat-square)](https://gratipay.com/PureKrome/)  
+![Lic: MIT](http://img.shields.io/badge/License-MIT-blue.svg?style=flat-square)
---
+++ StarterKit/Models/OAuthSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StarterKit
+{
+    public class OAuthSettings
+    {
+        public string PublicToken
+        {
+            get;
+            set;
+        }
+
+        public string SecretToken
+        {
+            get;
+            set;
+        }
+
+        public string Scopes
+        {
+            get;
+            set;
+        }
+
+        public string AuthorizeUrl
+        {
+            get;
+            set;
+        }
+
+        public string RedirectUrl
+        {
+            get;
+            set;
+        }
+    }
+}
